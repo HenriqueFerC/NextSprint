a7f1a052be81c38525ff9e7b273f8bd362fd8daf
Matheus da Silva Ferreira <56132266+MatheusFerreira05@users.noreply.github.com>
2023-11-13 22:02:06 -0300

    Add files via upload

---
+++ bd.py
@@ -0,0 +1,34 @@
+Chamado = [
+    {
+        'id': 1,
+        'nome': 'Henrique',
+        'placa': 'ABC1234',
+        'veiculo': 'civic',
+        'local': 'Avenida Lins',
+        'descricao': 'Carro Tombou'
+    },
+    {
+        'id': 2,
+        'nome': 'Matheus',
+        'placa': 'DEF4321',
+        'veiculo': 'jeep',
+        'local': 'Avenida Lins',
+        'descricao': 'Jeep Tombou e furou o pneu'
+    },
+    {
+        'id': 3,
+        'nome': 'Victor',
+        'placa': 'HIJ1243',
+        'veiculo': 'caminhão',
+        'local': 'São Paulo',
+        'descricao': 'Caminhão atolou'
+    },
+    {
+        'id': 4,
+        'nome': 'Lucas',
+        'placa':'KLM2413',
+        'veiculo': 'bis',
+        'local': 'heliopolis',
+        'descricao': 'Moto capotou'
+    },
+]
