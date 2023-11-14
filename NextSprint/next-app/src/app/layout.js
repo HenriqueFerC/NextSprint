a694bca68e9e27e8bd7c3db831c36a01bd7c90d6
Lucas de Lavor Andrade <61985933+lucaslavor22@users.noreply.github.com>
2023-11-13 22:16:02 -0300

    Subindo páginas do projeto

---
+++ NextSprint/next-app/src/app/layout.js
@@ -0,0 +1,20 @@
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Projeto Porto',
+  description: 'Sprint 4',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+            
+          {children}
+      </body>
+    </html>
+  )
+}
